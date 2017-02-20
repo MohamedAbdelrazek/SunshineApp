9636ccc79b78aabbcb98dfb197d969c8fd921de6
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2017-02-20 11:19:37 +0200

    Setting main Fragment and details Fragment &&Fetching data and parsing it

---
+++ app/src/main/java/com/zoka/sunshineapp/ForecastFragment.java
@@ -0,0 +1,8 @@
+package com.zoka.sunshineapp;
+
+/**
+ * Created by Mohamed AbdelraZek on 2/20/2017.
+ */
+
+public class ForecastFragment {
+}
---
+++ app/src/main/java/com/zoka/sunshineapp/utils/NetworkUtils.java
@@ -0,0 +1,8 @@
+package com.zoka.sunshineapp.utils;
+
+/**
+ * Created by Mohamed AbdelraZek on 2/19/2017.
+ */
+
+public class NetworkUtils {
+}
