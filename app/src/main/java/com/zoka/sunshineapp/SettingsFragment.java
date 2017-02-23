412c4c33a61136763081d161a583118932725dc7
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2017-02-24 01:22:51 +0200

    Display summary  for setting preference .

--- app/src/main/java/com/zoka/sunshineapp/SettingsFragment.java
+++ app/src/main/java/com/zoka/sunshineapp/SettingsFragment.java
@@ -1,15 +1,77 @@
 package com.zoka.sunshineapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Mohamed AbdelraZek on 2/24/2017.
  */
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_general);
+        //Log.i("ZOKA","onCreatePreferences");
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+
+        // Go through all of the preferences, and set up their preference summary.
+        for (int i = 0; i < count; i++) {
+            //Log.i("ZOKA","count =  "+count);
+            Preference p = prefScreen.getPreference(i);
+            if ((p instanceof EditTextPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+    }
+
+    private void setPreferenceSummary(Preference p, String value) {
+        if (p instanceof EditTextPreference) {
+            // For EditTextPreferences, set the summary to the value's simple string representation.
+            p.setSummary(value);
+            //  Log.i("ZOKA","setPreferenceSummary");
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // Figure out which preference was changed
+        //Log.i("ZOKA","onSharedPreferenceChanged");
+        Preference preference = findPreference(key);
+        if (null != preference) {
+            // Updates the summary for the preference
+            if ((preference instanceof EditTextPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+        //Log.i("ZOKA","OnCreate View !");
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //  Log.i("ZOKA","On destroy !");
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
